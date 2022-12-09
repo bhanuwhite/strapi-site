0e0b9f0a0e8720b0d1d613dc58bcc93869fdbfb3
shivam <shivam.ahex@gmail.com>
2022-12-09 19:09:57 +0530

    implement logout

--- Angular-Blog/src/app/apps/shared/main-header/main-header.component.ts
+++ Angular-Blog/src/app/apps/shared/main-header/main-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    localStorage.clear();
+    this.router.navigateByUrl('/login')
+  }
 }
