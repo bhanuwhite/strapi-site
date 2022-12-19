a49ca769c5d445f7ecebc1d2f80c9167c154c51f
angular <angular@gmail.com>
2022-12-19 11:07:32 +0530

    commit

--- Angular-Blog/src/app/apps/partners/partner/Api.service.ts
+++ Angular-Blog/src/app/apps/partners/partner/Api.service.ts
@@ -10,28 +10,28 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   post(data: any) {
-    return this.http.post<any>("baseurl", data)
+    return this.http.post<any>("https://d631-157-38-46-64.ngrok.io/api/partners/", data)
       .pipe(map((res: any) => {
         return res;
       }))
   }
 
   get() {
-    return this.http.get<any>("baseurl")
+    return this.http.get<any>("https://d631-157-38-46-64.ngrok.io/api/partners/")
       .pipe(map((res: any) => {
         return res;
       }))
   }
 
   update(data: any, id: number) {
-    return this.http.put<any>("baseurl" + id, data)
+    return this.http.put<any>("https://d631-157-38-46-64.ngrok.io/api/partners/" + id, data)
       .pipe(map((res: any) => {
         return res;
       }));
   }
 
   delete(id: number) {
-    return this.http.delete<any>("baseurl" + id)
+    return this.http.delete<any>("https://d631-157-38-46-64.ngrok.io/api/partners/" + id)
       .pipe(map((res: any) => {
         return res;
       }));
