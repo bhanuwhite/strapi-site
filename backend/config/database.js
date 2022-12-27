eed1845fc862a06633d5d7df5563e6f5c00fa62f
angular <angular@gmail.com>
2022-12-27 18:01:49 +0530

    changes has updated on backened

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "prac"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "root")
+      password: env("DATABASE_PASSWORD", "vamsi@8008")
     },
   }
 });
