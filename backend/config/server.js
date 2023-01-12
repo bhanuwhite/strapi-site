584634abbfaf4e9eec0df7646a109dbf0e4714c8
bhanuwhite <feather.white@gmail.com>
2023-01-12 16:29:51 +0530

    Updating connect build files

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '206.189.140.51'),
+  port: env.int('PORT', 3300),
   // url: 'https://3b9d-157-38-15-216.ngrok.io/',
-  url: 'http://localhost:1337',
+  url: 'http://206.189.140.51:3300',
   app: {
     keys: env.array('APP_KEYS'),
   },
